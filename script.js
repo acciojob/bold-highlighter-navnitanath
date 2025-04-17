ad36b99b4975f2b4a209351b1e7940aaedd21ad7
navnitanath <nancynath751@gmail.com>
2025-04-17 14:04:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function highlight() {
-    //Write your code here
-
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
+	document.querySelectorAll('strong').forEach(element =>{
+		element.style.color = "rgb(0, 128, 0)";
+	});
     
+}
+function return_normal() {
+	document.querySelectorAll('strong').forEach(element =>{
+		element.style.color = "rgb(0, 0, 0)";
+	});
+ 
 }
